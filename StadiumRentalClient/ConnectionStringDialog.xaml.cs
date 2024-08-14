0ae73732a7fa9a87dc752a55fcf9bad574022842
Tim Miller <tim_j_miller@yahoo.com>
2024-08-14 08:58:43 -0400

    Add project files.

---
+++ StadiumRentalClient/ConnectionStringDialog.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Driver.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StadiumRentalClient
+{
+    /// <summary>
+    /// Interaction logic for ConnectionStringDialog.xaml
+    /// </summary>
+    public partial class ConnectionStringDialog : UserControl
+    {
+        private static string? connectionString;
+        public ConnectionStringDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Connection_String_Textbox.Text != "")
+            {
+                connectionString = Connection_String_Textbox.Text;
+            }
+            MainWindow.Get_Key();
+
+            if (MainWindow.Test_Key())
+                Visibility = Visibility.Collapsed;
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public static string GetConnectionString()
+        {
+            if (connectionString != null)
+                return connectionString;
+            else
+                return "null";
+        }
+    }
+}
---
+++ StadiumRentalClient/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MongoDB.Driver;
+
+namespace StadiumRentalClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string? connectionString = "I ain't putting the connection string into a repo";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            ConnectionStringPopup.IsOpen = true;
+        }
+        public static void Get_Key()
+        {
+            connectionString = ConnectionStringDialog.GetConnectionString();
+        }
+        public static bool Test_Key()
+        {
+            MongoClient dbClient;
+            try
+            {
+                dbClient = new MongoClient(connectionString);
+            }
+            catch (MongoConfigurationException ex)
+            {
+                return false;
+            }
+            catch (MongoAuthenticationException ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
